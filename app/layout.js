21bb2fb6d204bb3ce360fa5a54d77842386f8560
Bence <m.bence211@gmail.com>
2024-11-27 20:23:44 +0100

    div nesting changes

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className="">
+				{/* flex flex-col justify-center items-center */}
 				<Header />
 
 				{children}
--- app/page.js
+++ app/page.js
@@ -2,5 +2,9 @@ import Header from "./_components/Header";
 import Main from "./_components/Main";
 
 export default function Home() {
-	return <Main />;
+	return (
+		<>
+			<Main />;
+		</>
+	);
 }
