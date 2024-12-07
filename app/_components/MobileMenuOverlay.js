8fd398d3e89f560ee8849fa44337405f58d9d7db
Bence <m.bence211@gmail.com>
2024-12-07 16:23:59 +0100

    added animation to the mobile menu

--- app/_components/MobileMenuOverlay.js
+++ app/_components/MobileMenuOverlay.js
@@ -1,11 +1,28 @@
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import Logo from "./Logo";
+import * as motion from "motion/react-client";
+import { useMotionValue, useTransform, animate } from "motion/react";
 
 export default function MobileMenuOverlay({ onClose }) {
+	const overlayVariants = {
+		transition: {
+			duration: 1,
+
+			ease: "linear",
+			times: [0, 0.5, 0.5, 1],
+		},
+	};
 	return (
-		<div className="fixed inset-0 z-50 bg-black bg-opacity-40">
-			<div className="relative  h-full w-full flex flex-col">
+		<motion.div
+			className="fixed inset-0 z-50 bg-black bg-opacity-95"
+			initial={{ x: 100 }}
+			animate={{ x: 0 }}
+			transition={{ duration: 0.3, ease: "easeInOut" }}
+		>
+			{/* <FocusLock> */}
+			{/* Focus lock to trap focus in also to stop scrolling (could use react-focus-on), issue: it doesn't have react 19 support yet.. Update later */}
+			<div className="relative  h-full w-full flex flex-col gap-24">
 				<div className="flex items-center justify-between px-6 py-4 border-b border-gray-200">
 					<Logo />
 
@@ -14,7 +31,7 @@ export default function MobileMenuOverlay({ onClose }) {
 					</button>
 				</div>
 
-				<ul className="flex flex-col justify-center items-center flex-grow">
+				<ul className="flex flex-col justify-start items-center flex-grow gap-24">
 					<li>
 						<Link href={"/"} className={`hover:text-hover text-lg py-2`}>
 							Home
@@ -43,6 +60,7 @@ export default function MobileMenuOverlay({ onClose }) {
 					</li>
 				</ul>
 			</div>
-		</div>
+			{/* </FocusLock> */}
+		</motion.div>
 	);
 }
