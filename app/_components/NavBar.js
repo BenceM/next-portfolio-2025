b2dfc2b0b077b0def6b4528bfac4cb18796df48b
Bence <m.bence211@gmail.com>
2024-12-11 15:51:24 +0100

    redesign of the mediaquery hook

--- app/_components/NavBar.js
+++ app/_components/NavBar.js
@@ -1,13 +1,15 @@
+"use client";
 import MobileMenu from "./MobileMenu";
 import DesktopMenu from "./DesktopMenu";
 import useMediaQuery from "../_hooks/useMediaQuery";
 
 export default function NavBar() {
+	// const isDesktop = useMediaQuery("768");
 	const isDesktop = useMediaQuery("(min-width: 768px)");
 
 	return (
 		<nav className="md:flex md:flex-wrap md:items-center md:justify-between mx-auto px-8 py-4 md:py-0 md:pl-14 md:fixed md:z-50 md:top-0 md:left-0 md:right-0 md:bg-background">
-			{isDesktop ? <MobileMenu /> : <DesktopMenu />}
+			{isDesktop ? <DesktopMenu /> : <MobileMenu />}
 		</nav>
 	);
 }
--- app/_hooks/useMediaQuery.js
+++ app/_hooks/useMediaQuery.js
@@ -1,19 +1,56 @@
+"use client";
 import { useState, useEffect } from "react";
 
+//3
 const useMediaQuery = (query) => {
 	const [matches, setMatches] = useState(false);
 
 	useEffect(() => {
+		if (typeof window === "undefined") return;
 		const media = window.matchMedia(query);
-		if (media.matches !== matches) {
-			setMatches(media.matches);
-		}
-		const listener = () => setMatches(media.matches);
-		window.addEventListener("resize", listener);
-		return () => window.removeEventListener("resize", listener);
-	}, [matches, query]);
+
+		const updateMatches = () => setMatches(media.matches);
+		updateMatches();
+
+		media.addEventListener("change", updateMatches);
+		return () => media.removeEventListener("change", updateMatches);
+	}, [query]);
 
 	return matches;
 };
 
 export default useMediaQuery;
+
+//1
+// const useMediaQuery = (query) => {
+// 	const [matches, setMatches] = useState(false);
+
+// 	useEffect(() => {
+// 		console.log(window.innerWidth);
+// 		const media = window.matchMedia(query);
+// 		console.log(media);
+// 		if (media.matches !== matches) {
+// 			setMatches(media.matches);
+// 		}
+// 		const listener = () => setMatches(media.matches);
+// 		window.addEventListener("resize", listener);
+// 		return () => window.removeEventListener("resize", listener);
+// 	}, [matches, query]);
+
+// 	return matches;
+// };
+//2
+// const useMediaQuery = (query) => {
+// 	const [isDesktop, setIsDesktop] = useState(false);
+// 	const [screenSize, setScreenSize] = useState(0);
+// 	useEffect(() => {
+// 		const listener = () => setScreenSize(window.innerWidth);
+// 		setScreenSize(window.innerWidth);
+
+// 		setIsDesktop(query <= screenSize ? true : false);
+// 		window.addEventListener("resize", listener);
+// 		return () => window.removeEventListener("resize", listener);
+// 	}, [query, screenSize]);
+
+// 	return isDesktop;
+// };
